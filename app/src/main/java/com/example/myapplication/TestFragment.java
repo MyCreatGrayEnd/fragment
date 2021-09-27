31103ead7c749301df1c0e653f1eefc06bb9843a
zhazhagou <826859987@qq.com>
2021-09-27 20:12:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/TestFragment.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public  class TestFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_black, container,false);
+        return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
